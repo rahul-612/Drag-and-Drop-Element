5e4a08f286dafa90693c0d260b2dd4d7d8d975be
Rahul <rk785164@gmail.com>
2021-07-28 22:46:10 +0530

    Added Comments

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ imgBox.addEventListener('dragstart', (e) => {
     e.target.className += ' hold';
     setTimeout(() => {
         e.target.className = 'hide';
-    })                                          //yeha humne setTimeout(0) isliye kiya kuki hum isko sbse end m chle
+    })                                          
     drag.play();
     
 });
 
 imgBox.addEventListener('dragend', (e) => {
     console.log('DragEnd has been triggered!');
-    e.target.className = 'imgBox';                //isse hold and hide wali property end ho jayegi dusre div m jb ye image drop hogi to
+    e.target.className = 'imgBox';                
 });
 
 for (whiteBox of whiteBoxes) {
@@ -39,7 +39,7 @@ for (whiteBox of whiteBoxes) {
 
     whiteBox.addEventListener('drop', (e) => {
         console.log('drop has been triggered!');
-        e.target.append(imgBox);                        //isse jis box mei hum rkhge wha append ho jayega
+        e.target.append(imgBox);                        
         drop.play();
     })
 }
